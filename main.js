98c0f94bcb5d3bf244f7f7430c8ecd271564b1e1
Dan Snyder <daniel.c.snyder@gmail.com>
2016-12-09 18:42:09 -0800

    Add complete click functionality

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', (e) => {
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) {
-    return;
-  }
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  // rewind audio to the start each click
   audio.currentTime = 0;
   audio.play();
-})
+  key.classList.add('playing');
+}
+
+const removeTransition = function (e) {
+  if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
